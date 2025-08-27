2d2c350f23aef6e4267604ce55ada92ff63f89d3
CalderonCodes <cgcalderondev@gmail.com>
2025-08-27 13:40:01 -0600

    :feat: add navbar hover menu

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,12 +1,16 @@
 import "./Navbar.scss";
 import {
   FaClock,
+  FaEllipsisH,
   FaFile,
+  FaFileUpload,
+  FaFolderPlus,
   FaHandHolding,
   FaHome,
   FaPlus,
   FaShareAlt,
   FaTrash,
+  FaUpload,
 } from "react-icons/fa";
 
 function Navbar() {
@@ -40,13 +44,35 @@ function Navbar() {
         </span>
         <hr className="divider" />
       </div>
+      <div className="navbar_wrapper">
+        <div className="navbar_wrapper_box">
+          <div className="navbar_actions">
+            <span className="navbar_actions_action">
+              <FaFileUpload className="navbar_navigation_icon" />
+              <p>Upload files</p>
+            </span>
+            <span className="navbar_actions_action">
+              <FaUpload className="navbar_navigation_icon" />
+              <p>Upload folder</p>
+            </span>
+            <span className="navbar_actions_action">
+              <FaFolderPlus className="navbar_navigation_icon" />
+              <p>New folder</p>
+            </span>
+            <span className="navbar_actions_action">
+              <FaEllipsisH className="navbar_navigation_icon" />
+              <p>More</p>
+            </span>
+          </div>
+        </div>
 
-      <button className="navbar_button">
-        <p className="navbar_button_text">Create New</p>
-        <span className="navbar_button_span">
-          <FaPlus className="navbar_button_icon" />
-        </span>
-      </button>
+        <button className="navbar_button">
+          <p className="navbar_button_text">Create New</p>
+          <span className="navbar_button_span">
+            <FaPlus className="navbar_button_icon" />
+          </span>
+        </button>
+      </div>
     </div>
   );
 }
