97413a4f21377dd4d9c375afc4cda7043cabf087
CalderonCodes <cgcalderondev@gmail.com>
2025-08-27 13:58:05 -0600

    :feat: add storage heaer

--- src/components/Storage/Storage.tsx
+++ src/components/Storage/Storage.tsx
@@ -1,9 +1,22 @@
-import './Storage.scss'
+import { FaCog, FaSignOutAlt, FaUser } from "react-icons/fa";
+import "./Storage.scss";
 
 function Storage() {
   return (
-    <div className='storage'>Storage</div>
-  )
+    <div className="storage">
+      <div className="storage_header">
+        <div className="storage_header_options">
+          <FaSignOutAlt className="storage_header_options_icon" />
+          <FaCog className="storage_header_options_icon" />
+        </div>
+
+        <span className="storage_header_avatar">
+          <p>Name</p>
+          <FaUser className="storage_header_avatar_icon" />
+        </span>
+      </div>
+    </div>
+  );
 }
 
-export default Storage
+export default Storage;
