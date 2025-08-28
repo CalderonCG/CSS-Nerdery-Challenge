e74329e5c795c764d445434bb3bd443a329f8005
CalderonCodes <cgcalderondev@gmail.com>
2025-08-28 12:53:56 -0600

    :feat: add recent files section

--- src/components/Recent/Recent.tsx
+++ src/components/Recent/Recent.tsx
@@ -3,6 +3,7 @@ import SearchBar from "../SearchBar/SearchBar";
 import "./Recent.scss";
 import UsedCard from "../UsedCard/UsedCard";
 import SharedCard from "../SharedCard/SharedCard";
+import RecentCard from "../RecentCard/RecentCard";
 function Recent() {
   return (
     <div className="recent">
@@ -16,14 +17,41 @@ function Recent() {
       </div>
 
       <div className="recent_used">
-        <UsedCard name="App Project" date="20.02.2020" amount={['neutral', 'neutral']} label={false} />
-        <UsedCard name="Project: fitbit" date="28.02.2020" amount={['neutral', 'neutral']} label={false}/>
-        <UsedCard name="Client documents" date="4.03.2020" amount={['neutral', 'neutral', 'neutral']}  label={true}/>
+        <UsedCard
+          name="App Project"
+          date="20.02.2020"
+          amount={["neutral", "neutral"]}
+          label={false}
+        />
+        <UsedCard
+          name="Project: fitbit"
+          date="28.02.2020"
+          amount={["neutral", "neutral"]}
+          label={false}
+        />
+        <UsedCard
+          name="Client documents"
+          date="4.03.2020"
+          amount={["neutral", "neutral", "neutral"]}
+          label={true}
+        />
       </div>
 
       <div className="recent_header">
         <h1>Recent files</h1>
         <p>View All</p>
+      </div>
+
+      <div className="recent_files">
+        <div className="recent_files_header">
+          <p>Name</p>
+          <p>Members</p>
+          <p>Last Modified</p>
+        </div>
+        <RecentCard name="Travel Landing Page" members={5} date="Mar 8, 2020" color="primary"/>
+        <RecentCard name="True Photos" members={12} date="Mar 8, 2020" color="green"/>
+        <RecentCard name="Dashboard Structure" members={10} date="Mar 9, 2020" color="red"/>
+        <RecentCard name="Character Illustration" members={3} date="Mar 10, 2020" color="primary"/>
       </div>
 
       <div className="recent_header">
@@ -32,11 +60,23 @@ function Recent() {
       </div>
 
       <div className="recent_used">
-        <SharedCard name="App Project" date="20.02.2020" amount={['green', 'red']} />
+        <SharedCard
+          name="App Project"
+          date="20.02.2020"
+          amount={["green", "red"]}
+        />
 
-        <SharedCard name="App Project" date="20.02.2020" amount={['green', 'red', 'yellow']}  />
+        <SharedCard
+          name="App Project"
+          date="20.02.2020"
+          amount={["green", "red", "yellow"]}
+        />
 
-        <SharedCard name="App Project" date="20.02.2020" amount={['green', 'yellow']}  />
+        <SharedCard
+          name="App Project"
+          date="20.02.2020"
+          amount={["green", "yellow"]}
+        />
       </div>
     </div>
   );
---
+++ src/components/RecentCard/RecentCard.tsx
@@ -0,0 +1,32 @@
+import { FaEllipsisH } from 'react-icons/fa'
+import './RecentCard.scss'
+import clsx from 'clsx';
+
+type RecentCardProps={
+    name: string;
+    members: number;
+    date: string;
+    color: 'green' | 'red' | 'primary'
+}
+
+function RecentCard({name, members, date, color} : RecentCardProps) {
+  return (
+    <div className='recent_card'>
+        <div className='recent_card_name'>
+            <div className={clsx('recent_card_name_square',
+                {
+                    recent_red: color === 'red',
+                    recent_green: color === 'green'
+                }
+            )}/>
+            <p>{name}</p>
+        </div>
+        <p>{members} members</p>
+        <p>{date}</p>
+
+        <FaEllipsisH/>
+    </div>
+  )
+}
+
+export default RecentCard
