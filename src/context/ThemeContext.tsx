2e0c173f0814f9d9fcc63f6e53a352c7e6c64077
CalderonCodes <cgcalderondev@gmail.com>
2025-08-29 11:00:58 -0600

    :feat: add sidebar transitions

--- src/context/ThemeContext.tsx
+++ src/context/ThemeContext.tsx
@@ -18,7 +18,7 @@ export const ThemeContext = createContext<ThemeContextType>({
 
 //Context provider------------------------------------------
 export const ThemeProvider = ({ children }: { children: ReactNode }) => {
-  const [theme, setTheme] = useState<Theme>("dark");
+  const [theme, setTheme] = useState<Theme>("light");
 
   //When theme changes it sets the data-theme
   useEffect(() => {
